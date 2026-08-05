0d1a0bbed17f6b2c865b7967e534859f6dfc4e21
maik <michaelaykay@gmail.com>
2026-08-04 21:28:48 -0700

    added verbose, to check additional tags and return metadatas

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    
+        
     messages = [
         {
             "role": "user",
@@ -29,15 +30,17 @@ def main():
     
     response = client.chat.completions.create(model="openrouter/free", messages=messages)
 
-    message_dict = messages[0]
-    print(f"User prompt: {message_dict.get('content')}")
-
     if response is None:
         raise RuntimeError("No response generated ")
+
+    if args.verbose:
+        message_dict = messages[0]
+        print(f"User prompt: {message_dict.get('content')}")
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+    else:
+        print(f"Response: {response.choices[0].message.content}")
         
-    print(f"Prompt tokens: {response.usage.prompt_tokens}")
-    print(f"Response tokens: {response.usage.completion_tokens}")
-    print(f"Response: {response.choices[0].message.content}")
-    
 if __name__ == "__main__":
     main()
